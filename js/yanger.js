320e11aa7fa298a67170e219cfdebe13cf30ac56
Kodoku-Shinku <germanzoku@gmail.com>
2020-06-05 13:00:39 -0500

    correcting typewrite error in js

--- js/yanger.js
+++ js/yanger.js
@@ -31,16 +31,6 @@ $().ready(function(){
 
     });
 
-    function rotateCard(rotate-btn){
-        var $card = $(rotate-btn).closest('.card-container');
-        console.log($card);
-        if($card.hasClass('hover')){
-            $card.removeClass('hover');
-        } else {
-            $card.addClass('hover');
-        }
-    }
-
 $('.card-wrapper').click(function(){ //hover  can be used
         $(this).find('.card').toggleClass('flipped');
 
