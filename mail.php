d11c8a421c0bb1ed494032d6b1a0dafa97906ec9
Kodoku-Shinku <germanzoku@gmail.com>
2020-06-15 21:59:42 -0500

    implement contact.html view

--- mail.php
+++ mail.php
@@ -26,7 +26,7 @@ print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
 exit();
 }
 $content="From: $name \nEmail: $email \nMessage: $message";
-$recipient = "youremail@here.com";
+$recipient = "contact@soufunstudio.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
