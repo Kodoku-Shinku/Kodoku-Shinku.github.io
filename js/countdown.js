89ec9ab9fe5f8cb859eceab896d7c93fa56b1abd
Kodoku-Shinku <germanzoku@gmail.com>
2020-06-26 15:01:17 -0500

    implement projects.html view

--- js/countdown.js
+++ js/countdown.js
@@ -1,31 +1,53 @@
-function countdown(totalDays) {
-  // Set the date we're counting down to
-  var countDownDate = new Date();
-  countDownDate.setDate(countDownDate.getDate() + totalDays);
+$(function () {
 
-  // Update the count down every 1 second
-  var x = setInterval(function() {
 
-    // Get todays date and time
-    var now = new Date().getTime();
+    /* =========================================
+        COUNTDOWN 1
+     ========================================= */
+    $('#clock-a').countdown('2020/9/15').on('update.countdown', function(event) {
+      var $this = $(this).html(event.strftime(''
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%D</span> Day%!d</div>'
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%H</span> Hr</div>'
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%M</span> Min</div>'
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%S</span> Sec</div>'));
+    });
 
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
+    /* =========================================
+        COUNTDOWN 2
+     ========================================= */
+    $('#clock-b').countdown('2020/12/22').on('update.countdown', function(event) {
+      var $this = $(this).html(event.strftime(''
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%D</span> Day%!d</div>'
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%H</span> Hr</div>'
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%M</span> Min</div>'
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%S</span> Sec</div>'));
+    });
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the element with id="demo"
-    document.getElementById("time-counter").innerHTML = days + "d " + hours + "h " +
-      minutes + "m " + seconds + "s ";
-
-    // If the count down is finished, write some text
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("time-counter").innerHTML = "EXPIRED";
+    /* =========================================
+        COUNTDOWN 4
+     ========================================= */
+    function get15dayFromNow() {
+        return new Date(new Date().valueOf() + 15 * 24 * 60 * 60 * 1000);
     }
-  }, 1000);
-}
+
+    $('#clock-c').countdown(get15dayFromNow(), function(event) {
+      var $this = $(this).html(event.strftime(''
+        + '<span class="h1 font-weight-bold">%D</span> Day%!d'
+        + '<span class="h1 font-weight-bold">%H</span> Hr'
+        + '<span class="h1 font-weight-bold">%M</span> Min'
+        + '<span class="h1 font-weight-bold">%S</span> Sec'));
+    });
+    /* =========================================
+        CALL TO ACTIONS FOR COUNTDOWN
+     ========================================= */
+    $('#btn-reset').click(function() {
+        $('#clock-c').countdown(get15dayFromNow());
+    });
+    $('#btn-pause').click(function() {
+        $('#clock-c').countdown('pause');
+    });
+    $('#btn-resume').click(function() {
+        $('#clock-c').countdown('resume');
+    });
+
+});
