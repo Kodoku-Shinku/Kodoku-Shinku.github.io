efbb8c0c9c3a5fc8a010c606c643b8170502fd9a
Kodoku-Shinku <germanzoku@gmail.com>
2020-06-27 19:28:00 -0500

    implement index.html view

--- js/yanger.js
+++ js/yanger.js
@@ -1,46 +1,48 @@
-'use strict';
+(function($) {
+  "use strict"
 
-$(window).on('load', function() {
-	/*------------------
-		Preloader
-	--------------------*/
-	$(".loader").fadeOut();
-	$("#preloader").delay(400).fadeOut("slow");
+  /* 1. Preloader */
+  $(window).on('load', function() {
+    $('#preloader-active').delay(450).fadeOut('slow');
+    $('body').delay(450).css({
+      'overflow': 'visible'
+    });
+  });
 
-});
 
-
-$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function () {
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-
-  for (var i = 0; i < 4; i++) {
-    next = next.next();
+  $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function() {
+    var next = $(this).next();
     if (!next.length) {
       next = $(this).siblings(':first');
     }
     next.children(':first-child').clone().appendTo($(this));
-  }
-});
 
-$().ready(function(){
-        $('[rel="tooltip"]').tooltip();
+    for (var i = 0; i < 4; i++) {
+      next = next.next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
 
-    });
+  $().ready(function() {
+    $('[rel="tooltip"]').tooltip();
 
-//Rotating-cards
-$('.rotate-btn').click(function(){ //hover  can be used
-		  var id = $(this).attr('data-card');
-      $('#'+id).toggleClass('do-flip');
+  });
 
-});
+  //Rotating-cards
+  $('.rotate-btn').click(function() { //hover  can be used
+    var id = $(this).attr('data-card');
+    $('#' + id).toggleClass('do-flip');
 
-// popovers Initialization
-$(function () {
-$('[data-toggle="popover"]').popover()
-})
+  });
 
-new WOW().init();
+  // popovers Initialization
+  $(function() {
+    $('[data-toggle="popover"]').popover()
+  })
+
+  new WOW().init();
+
+})(jQuery);
